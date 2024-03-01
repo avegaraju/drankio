d15d60c955dd1ab426790dbbcb899493dc5f2411
Ashish Vegaraju <ashish_vegaraju@yahoo.com>
2024-03-02 00:53:49 +0100

    Change theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import "../src/css/style.default.css";
+import "../src/css/style.red.css";
 import "../src/css/custom.css";
 import "../src/css/fontastic.css";
 import "../src/vendor/bootstrap/css/bootstrap.min.css";
 import "../src/vendor/font-awesome/css/font-awesome.min.css";
 import Header from "../src/components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="page">
       <Header />
+      <Main />
     </div>
   );
 }
